450fdd92cd65a72800d70f4f14e5bde0d460cd7b
Alec Glassford <glassford@stanford.edu>
2017-12-01 17:47:02 -0800

    👨🏻‍🍳 You know what, why not just be manifestly clear (or whatever)

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import fs from 'fs';
 import { csvParse } from 'd3-dsv';
 
 export default function (csvPath, rowParser) {
-  const str = fs.readFileSync(csvPath, 'utf-8');
-  return csvParse(str, rowParser);
+  const csvString = fs.readFileSync(csvPath, 'utf-8');
+  return csvParse(csvString, rowParser);
 }
