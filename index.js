b841ffc5c6413493f0f9efa7e70465f8f5b269b1
Alec Glassford <glassford@stanford.edu>
2018-03-05 12:24:29 -0800

    🔧 Fix encoding typo

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import fs from 'fs';
 import { csvParse } from 'd3-dsv';
 
 export default function (csvPath, rowParser) {
-  const csvString = fs.readFileSync(csvPath, 'utf-8');
+  const csvString = fs.readFileSync(csvPath, 'utf8');
   return csvParse(csvString, rowParser);
 }
